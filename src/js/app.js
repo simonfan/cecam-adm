080e62d9c43dbf1b55cf1296effcbc70b7786de5
Simon <simonxd@gmail.com>
2016-02-23 18:40:19 -0300

    navigation

--- src/js/app.js
+++ src/js/app.js
@@ -9,12 +9,18 @@ angular.module('cecamAdm', [
 
   $stateProvider
 
+
+  .state('home', {
+    url: '/',
+    templateUrl: 'templates/home.html'
+  })
+
   .state('estoque', {
     url: '/estoque',
     templateUrl: 'templates/estoque.html',
     controller: 'EstoqueCtrl',
-  })
-
+  });
+  
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/estoque');
 
