d71b0b870cddf5eb90a4caa40512f21ebd33b998
Simon <simonxd@gmail.com>
2016-04-14 12:37:15 -0300

    loading

--- src/js/app.js
+++ src/js/app.js
@@ -32,4 +32,46 @@ angular.module('cecamAdm', [
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/estoque');
 
-});
+})
+
+// http interceptor that shows loading indicator on ajax request
+.config(['$httpProvider', function($httpProvider) {
+
+  // alternatively, register the interceptor via an anonymous factory
+  $httpProvider.interceptors.push(function($q, $rootScope) {
+    return {
+      request: function(config) {
+
+        $rootScope.isLoading = true;
+
+        return config;
+      },
+
+      // optional method
+      requestError: function(rejection) {
+        // do something on error
+        
+        $rootScope.isLoading = false;
+
+        return $q.reject(rejection);
+      },
+
+      response: function(response) {
+
+        $rootScope.isLoading = false;
+
+        // same as above
+        return response;
+      },
+
+      // optional method
+      responseError: function(rejection) {
+        // do something on error
+        $rootScope.isLoading = false;
+
+        return $q.reject(rejection);
+      }
+    };
+  });
+}]);
+
